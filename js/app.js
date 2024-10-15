42513b0c14e6edf08f40d5f831fff641ed143cfe
jose luis condori <jose.l.condori@gmail.com>
2024-10-14 22:37:38 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.getElementById('btn').addEventListener('click', toggleBtn);
+
+function toggleBtn() {
+    const light = document.getElementById('light');
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+
+   
+}
